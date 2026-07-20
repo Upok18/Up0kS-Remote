f7332b7498c04c15e14519cb2f066e1091817517
Up0k <zubaerru08@gmail.com>
2026-07-20 19:07:47 +0600

    Updated version

--- PC/remote/commands/power.py
+++ PC/remote/commands/power.py
@@ -5,43 +5,18 @@ Shutdown Command
 from __future__ import annotations
 
 import subprocess
-from remote.logger import info
-from remote.version import PROTOCOL_VERSION
 
 
-def shutdown(remote, data: dict) -> dict:
-    """Shutdown the PC."""
-
-    info("Shutdown command received.")
+def shutdown():
 
     subprocess.run(
         ["shutdown", "/s", "/f", "/t", "500"],
         check=False,
     )
 
-    return {
-        "version": PROTOCOL_VERSION,
-        "type": "success",
-        "data": {
-            "message": "PC will shut down in 30 seconds."
-            },
-    }
-
-
-def restart(remote, data: dict) -> dict:
-    """Restart the PC."""
-
-    info("Restart command received.")
+def restart():
 
     subprocess.run(
         ["shutdown", "/r", "/f", "/t", "500"],
         check=False,
     )
-
-    return {
-        "version": PROTOCOL_VERSION,
-        "type": "success",
-        "data": {
-            "message": "PC will restart in 30 seconds."
-            },
-    }
