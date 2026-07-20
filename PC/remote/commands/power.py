4d8c57d7df83ff8f8f2bf0182117d9f0d712c838
Up0k <zubaerru08@gmail.com>
2026-07-20 19:35:55 +0600

    Power bug fix

--- PC/remote/commands/power.py
+++ PC/remote/commands/power.py
@@ -6,17 +6,35 @@ from __future__ import annotations
 
 import subprocess
 
+from remote.version import PROTOCOL_VERSION
 
-def shutdown():
+
+def shutdown(remote, data):
 
     subprocess.run(
         ["shutdown", "/s", "/f", "/t", "500"],
         check=False,
     )
 
-def restart():
+    return {
+        "version": PROTOCOL_VERSION,
+        "type": "success",
+        "data": {
+            "message": "Shutdown started."
+        },
+    }
+
+def restart(remote, data):
 
     subprocess.run(
         ["shutdown", "/r", "/f", "/t", "500"],
         check=False,
     )
+
+    return {
+        "version": PROTOCOL_VERSION,
+        "type": "success",
+        "data": {
+            "message": "Restart started."
+        },
+    }
